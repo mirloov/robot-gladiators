4c95c42e7b8b83f28a529466fe4a7a1c3b967e5a
Mirloov <mirloov@gmail.com>
2020-11-28 19:43:14 -0800

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -49,25 +49,54 @@ var fight = function(enemyName) {
   }
 };    
 
-for (var i = 0; i < enemyNames.length; i++) {
-  if (playerHealth > 0) {
+//function to start a new game
+var startGame = function() {
+  //reser player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
     // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
+      var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+      enemyHealth = 50;
 
     // use debugger to pause script from running and check what's going on at that moment in the code
     // debugger;
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedEnemyName);
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in batle! Game Over");
+      break;
+    }
+  }
+  //play again
+  endGame();
+};
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
   }
   else {
-    window.alert("You have lost your robot in batle! Game Over");
-    break;
+  window.alert("You've lost your robot in battle.");
   }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+if (playAgainConfirm) {
+  startGame();
 }
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+}
+//start the game when the page loads
+startGame();
+//function to end the entire game
+
